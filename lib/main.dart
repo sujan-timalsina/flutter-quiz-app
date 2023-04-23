86467cbe4e16bd203d18edd020f4a54b7d16e7c3
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-04-23 21:44:19 +0545

    Quiz starting menu designed

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_body.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientBody(),
+      ),
+    ),
+  );
+}
